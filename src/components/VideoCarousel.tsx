f8b347f9ec4a47046ca7ed2afda9ee81913ca5f8
saeid-tavazani <saeidtavazani@gmail.com>
2024-05-23 22:03:20 +0330

    add videoRef

--- src/components/VideoCarousel.tsx
+++ src/components/VideoCarousel.tsx
@@ -1,6 +1,54 @@
+import { useEffect, useRef, useState, MutableRefObject } from "react";
 import { hightlightsSlides } from "../constants";
+import gsap from "gsap";
+
+type videoType = {
+  isEnd: boolean;
+  startPlay: boolean;
+  videoId: number;
+  isLastVideo: boolean;
+  isPlaying: boolean;
+};
+
+// React.MutableRefObject<HTMLVideoElement[]>
 
 const VideoCarousel = () => {
+  const videoRef = useRef<HTMLVideoElement[] | null>([]);
+  const videoSpanRef = useRef<HTMLSpanElement[]>([]);
+  const videoDivRef = useRef<HTMLSpanElement[]>([]);
+
+  const [loadedData, setLoadedData] = useState<any[]>([]);
+  const [video, setVideo] = useState<videoType>({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  });
+
+  const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video;
+  useEffect(() => {
+    if (loadedData.length > 3) {
+      if (!isPlaying && videoRef.current) {
+        videoRef.current[videoId].pause();
+      } else if (startPlay && videoRef.current) {
+        startPlay && videoRef.current[videoId].play();
+      }
+    }
+  }, [startPlay, videoId, isPlaying]);
+
+  useEffect(() => {
+    const currentProgress = 0;
+    let span = videoSpanRef.current;
+
+    if (span[videoId]) {
+      let anim = gsap.to(span[videoId], {
+        onUpdate: () => {},
+        onComplete: () => {},
+      });
+    }
+  }, [videoId, startPlay]);
+
   return (
     <>
       <div className="flex items-center">
@@ -8,7 +56,21 @@ const VideoCarousel = () => {
           <div key={list.id} id="slider" className="sm:pr-20 pr-10">
             <div className="video-carousel_container">
               <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
-                <video id="video" playsInline={true} preload="auto" muted>
+                <video
+                  id="video"
+                  playsInline={true}
+                  preload="auto"
+                  muted
+                  ref={(el) =>
+                    el && videoRef.current ? (videoRef.current[i] = el) : null
+                  }
+                  onPlay={() => {
+                    setVideo((prevVideo) => ({
+                      ...prevVideo,
+                      isPlaying: true,
+                    }));
+                  }}
+                >
                   <source src={list.video} type="video/mp4" />
                 </video>
               </div>
@@ -22,6 +84,22 @@ const VideoCarousel = () => {
             </div>
           </div>
         ))}
+      </div>
+      <div className="relative flex-center mt-10">
+        <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full gap-2">
+          {videoRef.current?.map((_, i) => (
+            <span
+              className="w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer"
+              key={i}
+              ref={(el) => (el ? (videoDivRef.current[i] = el) : null)}
+            >
+              <span
+                className="absolute w-full h-full rounded-full"
+                ref={(el) => (el ? (videoSpanRef.current[i] = el) : null)}
+              />
+            </span>
+          ))}
+        </div>
       </div>
     </>
   );
