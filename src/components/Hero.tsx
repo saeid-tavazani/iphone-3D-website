3bd988296475d95cebd1bc4a840d9ea97f03b957
saeid-tavazani <saeidtavazani@gmail.com>
2024-05-23 20:11:09 +0330

    fix hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,6 +8,7 @@ const Hero = () => {
   );
   useGSAP(() => {
     gsap.to("#hero", { opacity: 1, delay: 1.5 });
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 1.5 });
   }, []);
 
   const handleVideoSrcSet = () => {
@@ -42,6 +43,15 @@ const Hero = () => {
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
