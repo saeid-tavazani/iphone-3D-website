f53411f732071b902b803b0bc11b368bcaac6997
saeid-tavazani <saeidtavazani@gmail.com>
2024-05-23 20:27:44 +0330

    fix heading

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,5 +1,34 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { rightImg, watchImg } from "../utils";
+
 const Highlights = () => {
-  return <div>Highlights</div>;
+  useGSAP(() => {
+    gsap.to("#title", { opacity: 1, y: 0 });
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
+  }, []);
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full bg-zinc common-padding"
+    >
+      <div className="mb-12 w-full items-end justify-between md:flex">
+        <h1 id="title" className="section-heading">
+          Get the highlights.
+        </h1>
+        <div className="flex flex-wrap items-end gap-5">
+          <p className="link">
+            Watch the film
+            <img src={watchImg} alt="watch" className="ml-2" />
+          </p>
+          <p className="link">
+            Watch the event
+            <img src={rightImg} alt="event" className="ml-2" />
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Highlights;
