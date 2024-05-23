81bf5600abc81b5d0f54df02fbebe52748b04a38
saeid-tavazani <saeidtavazani@gmail.com>
2024-05-23 20:52:46 +0330

    fix cart VideoCarousel

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./VideoCarousel";
 
 const Highlights = () => {
   useGSAP(() => {
@@ -12,21 +13,24 @@ const Highlights = () => {
       id="highlights"
       className="w-screen overflow-hidden h-full bg-zinc common-padding"
     >
-      <div className="mb-12 w-full items-end justify-between md:flex">
-        <h1 id="title" className="section-heading">
-          Get the highlights.
-        </h1>
-        <div className="flex flex-wrap items-end gap-5">
-          <p className="link">
-            Watch the film
-            <img src={watchImg} alt="watch" className="ml-2" />
-          </p>
-          <p className="link">
-            Watch the event
-            <img src={rightImg} alt="event" className="ml-2" />
-          </p>
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end justify-between md:flex">
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="event" className="ml-2" />
+            </p>
+          </div>
         </div>
       </div>
+      <VideoCarousel />
     </section>
   );
 };
